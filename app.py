5e03f2c2a7948f9c0d48210c9a31e8e4dce007ac
yaya-1302 <tiaraoktaviani40@gmail.com>
2022-08-27 16:48:09 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import json
+
+# flask read views in "templates" folder
+# flask read asset in "static" folder
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('/views/dashboard.html')
+
+
+@app.route('/accordions')
+def to_accordions():
+    return render_template('/views/accordions.html')
+
+
+@app.route('/gallery')
+def to_gallery():
+    return render_template('/views/gallery.html')
+
+
+@app.route('/formcontrol')
+def to_form():
+    return render_template('/views/basic_elements.html')
+
+# code started here
+
+
+@app.route('/demo')
+def demo_page():
+    return render_template('/views/demo.html')
+
+# =======================================================
+# logic started here
+
+
+@app.route('/identify', methods=['GET'])
+def san_headline_identification():
+    judul = request.args.get('title')
+    pengolahan = request.args.get('model')
+
+    is_cb = is_clickbait(judul, pengolahan)
+    return json.dumps(
+        {
+            'judul': judul,
+            'is_clickbait': is_cb
+        }
+    ), 200, {'ContentType': 'application/json'}
+
+
+def is_clickbait(judul, model):
+    identify = judul
+    return True
+
+
+app.run(debug=True)
---
+++ static/js/main/demo.js
@@ -0,0 +1,43 @@
+$(function () {
+
+  // Submit Onclick Event
+  $('#identify').on('click', function () {
+    // get input "headline news"
+    let title = $('#headline').val();
+    let model = $('input[name="model"]:checked').val()
+
+    // compute SAN from ajax with title as data
+
+    $.ajax({
+      type: 'GET',
+      url: "/identify",
+      data: {
+        'title': title,
+        'model': model
+      },
+      dataType: "json",
+      error: function (xhr) {
+        alert('something wrong')
+      },
+      success: function (res) {
+        $('#result-section').prop('hidden', false);
+        $('#result-judul').html(res.judul)
+
+        $('#prediksi').removeClass('bg-danger');
+        $('#prediksi').removeClass('bg-success');
+        if (res.is_clickbait) {
+          $('#prediksi').addClass('bg-danger');
+          $('#prediksi').find('.text').html('CLICK BAIT')
+        } else {
+          $('#prediksi').addClass('bg-success');
+          $('#prediksi').find('.text').html('BUKAN CLICK BAIT')
+        }
+        $('html, body').animate({
+          scrollTop: $("#prediksi").offset().top
+        }, 0);
+      }
+    })
+
+  })
+  //End
+})
